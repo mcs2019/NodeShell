114371c5c684f4ffb257b31ad01fd9c50bd362c3
Marcos Santanna <test_123@TEST-123s-MacBook-Air.local>
2019-09-23 12:54:55 -0400

    one more

--- bash.js
+++ bash.js
@@ -2,9 +2,21 @@
 process.stdout.write('prompt > ');
 
 // The sdtin 'data' event fires after a user types in a line
-process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim(); // remove the newLine
+// process.stdin.on('data', (data) => {
+//     const cmd = data.toString().trim(); // remove the newLine
 
+//     process.stdout.write('you typed: ' + cmd);
+//     process.stdout.write('\nprompt > ');
+// });
+
+process.stdin.on('data', (data) => {
+    let cmd = data.toString().trim(); // remove the newLine
+    if(cmd === 'pwd'){
+        cmd = process.cwd();
+        process.stdout.write(cmd);
+        process.stdout.write('\nprompt > ');
+    } else {
     process.stdout.write('you typed: ' + cmd);
     process.stdout.write('\nprompt > ');
+    }
 });
